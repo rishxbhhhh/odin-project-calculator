7914d55346703e39001c389937c64d670e0884f1
Rishabh Rajpurohit <rajpurohitrishabh1@gmail.com>
2022-01-29 23:50:55 +0530

    calculator working but without keyboard support

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-function operate(operand1, operand2, operator){
-
+let screen = document.querySelector(".screen");
+btn = document.querySelectorAll(".grid-item");
+let screenValue = "";
+for (item of btn) {
+  item.addEventListener("click", function (e) {
+    btnText = e.target.innerHTML;
+    if ((btnText == ".")) {
+      if (screenValue.includes(".") === false) {
+        screenValue += btnText;
+        screen.value = screenValue;
+      }
+    } else if (btnText == "x") {
+      btnText = "*";
+      screenValue += btnText;
+      screen.value = screenValue;
+    } else if (btnText == "=") {
+      screenValue = eval(screenValue);
+      screen.value = screenValue;
+    } else if (btnText == "C") {
+      screen.value = "";
+      screenValue = "";
+    } else if (btnText == "Del") {
+      screenValue = screenValue.slice(0, -1);
+      screen.value = screenValue;
+    } else if (btnText == "+/-") {
+      screenValue = eval(screenValue) * -1;
+      screen.value = screenValue;
+    } else if (btnText == "%") {
+      screen.value = eval(screenValue) / 100;
+      screenValue = "";
+    } else {
+      screenValue += btnText;
+      screen.value = screenValue;
+    }
+  });
 }
